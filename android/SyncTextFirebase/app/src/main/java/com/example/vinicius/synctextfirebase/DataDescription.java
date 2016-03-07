5ff064314ce42cc5d93fa8b083b68276a8f6bcaa
Vinícius Lima <viniciusb80@gmail.com>
2016-03-07 13:22:44 -0300

    Updating.

---
+++ android/SyncTextFirebase/app/src/main/java/com/example/vinicius/synctextfirebase/DataDescription.java
@@ -0,0 +1,29 @@
+package com.example.vinicius.synctextfirebase;
+
+public class DataDescription {
+    private String fileName;
+    private long fileSize;
+    private String data64Encoded;
+
+    @SuppressWarnings("unused")
+    private DataDescription(){
+    }
+
+    DataDescription(String fileName, long fileSize, String data64Encoded) {
+        this.fileName = fileName;
+        this.data64Encoded = data64Encoded;
+        this.fileSize = fileSize;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public long getFileSize() {
+        return fileSize;
+    }
+
+    public String getData64Encoded() {
+        return data64Encoded;
+    }
+}
