d363e6d31bd2015ce97600823e4e378b38450bf1
Vinicius Lima <vinicius@infomach.local>
2019-05-15 14:57:03 -0300

    Changing script to use the same SSH command for firmware version 5 and 6

--- python/python-tests/sonicwall_get_license.py
+++ python/python-tests/sonicwall_get_license.py
@@ -8,22 +8,15 @@ from paramiko.client import SSHClient
 
 warnings.filterwarnings(action='ignore', module='.*paramiko.*')
 
+# Licenses to look for
 licenses = {
-    # Licenses to look for on firmware version 5
-    '5': {
-        1: 'SSL VPN Nodes/Users',
-        2: 'Virtual Assist Nodes/Users',
-        3: 'Gateway Anti-Virus',
-        4: 'Anti-Spyware',
-        5: 'Intrusion Prevention',
-        6: 'Application Firewall',
-        7: 'Visualization Control'
-    },
-    # Licenses to look for on firmware version 6
-    '6': {
-        1: 'Gateway AV/Anti-Spyware/Intrusion Prevention/App Control/App Visualization',
-        2: 'Premium Content Filter'
-    }
+    1: 'SSL VPN Nodes/Users',
+    2: 'Virtual Assist Nodes/Users',
+    3: 'Gateway Anti-Virus',
+    4: 'Anti-Spyware',
+    5: 'Intrusion Prevention',
+    6: 'Application Firewall',
+    7: 'Visualization Control'
 }
 
 
@@ -42,17 +35,17 @@ def wait_prompt(timeout):
         time.sleep(1)
 
 
-# Searches licenses onto firmware version 5
-def search_licenses_v5(timeout):
+# Searches licenses
+def search_licenses(timeout):
     cont = 0
-    remaining_licenses = len(licenses['5'])
+    remaining_licenses = len(licenses)
     licenses_status = dict()
 
     while cont < timeout and remaining_licenses > 0:
         buff = channel.recv(1024)
         line = buff.decode('utf-8')
 
-        for key, license in licenses.get(credentials[host]['version'], {}).items():
+        for key, license in licenses.items():
             idx = line.find(license)  # Is it the license we are looking for?
             if idx != -1:  # License was found
                 idx += len(license) + 1
@@ -71,47 +64,6 @@ def search_licenses_v5(timeout):
             if chr(c) == '>':
                 cont = timeout
                 break
-        time.sleep(1)
-
-    return licenses_status
-
-
-# Searches licenses onto firmware version 6
-def search_licenses_v6(timeout):
-    cont = 0
-    remaining_licenses = len(licenses['6'])
-    licenses_status = dict()
-
-    while cont < timeout and remaining_licenses > 0:
-        buff = channel.recv(1024)
-        line = buff.decode('utf-8')
-        line = line[7:]  # Remove trash from line begining
-        line = ''.join(line)
-
-        for key, license in licenses['6'].items():
-            line = ''.join(line)
-            if line.startswith(license):  # Is it the license we are looking for?
-                line = line[len(license):]
-                line = line.split(' ')
-                for idx in range(len(line)):
-                    if line[idx] == 'Licensed' and idx > 0:  # Is the license valid?
-                        if line[idx - 1] == 'Not':
-                            licenses_status[key] = 'Not Licensed'
-                        else:
-                            licenses_status[key] = 'Licensed'
-                        break
-                remaining_licenses -= 1
-
-        cont += 1
-        if cont >= timeout:
-            raise TimeoutError
-        for c in buff:
-            if chr(c) == '>':
-                cont = timeout
-                break
-        line = ''.join(line)
-        if line.endswith('--MORE--'):
-            channel.send('\n')
         time.sleep(1)
 
     return licenses_status
@@ -156,14 +108,9 @@ for host in credentials.keys():
         channel = client.invoke_shell()
         wait_prompt(30)
 
-        if credentials[host]['version'] == '5':
-            channel.send('show status\n')
-            licenses_status[host] = search_licenses_v5(30)
-        elif credentials[host]['version'] == '6':
-            channel.send('show license status\n')
-            licenses_status[host] = search_licenses_v6(30)
+        licenses_status[host] = search_licenses(30)
 
-        for key, license in licenses.get(credentials[host]['version'], {}).items():
+        for key, license in licenses.items():
             print(license, '|', licenses_status.get(host).get(key))
         print('==============================')
 
@@ -173,14 +120,14 @@ for host in credentials.keys():
         client.close()
     except TimeoutError:
         licenses_status[host] = dict()
-        for key in range(1, len(licenses[credentials[host]['version']]) + 1):
+        for key in range(1, len(licenses) + 1):
             licenses_status[host][key] = 'Unreachable'
 
-        for license in licenses[credentials[host]['version']].values():
+        for license in licenses.values():
             print(license, '| Unreachable')
         print('==============================')
 
-# Writting output file
+# Writing output file
 write_directory = argv[2]
 with open(path_join(write_directory, 'sonicwall_licenses_status.txt'), 'w') as output_file:
     for host in licenses_status.keys():
